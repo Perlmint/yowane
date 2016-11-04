1bfecdec90389b95f2ef38908ce4e1b3f2809aa3
Gyusun Yeom <omniavinco@gmail.com>
2016-11-04 18:21:44 +0900

    set default theme for [a-z]

--- src/client.ts
+++ src/client.ts
@@ -9,10 +9,32 @@ $(document).ready(() => {
         const configStr = $("#oritatami-input").val();
         const config = JSON.parse(configStr) as OritatamiConfig;
         renderer = new Renderer(500, 500, 100, new Theme({
-            a: "red",
-            b: "lime",
-            c: "#1E90FF",
-            d: "#FF1493"
+            a: "#a69dd8",
+            b: "#0c35b0",
+            c: "#f82750",
+            d: "#0ebd31",
+            e: "#5fab4f",
+            f: "#c538cf",
+            g: "#014a59",
+            h: "#e14af8",
+            i: "#9fb730",
+            j: "#4bec60",
+            k: "#ef9345",
+            l: "#d2ece0",
+            m: "#9cda80",
+            n: "#dbc07c",
+            o: "#7328dd",
+            p: "#1e9942",
+            q: "#621b7b",
+            r: "#c830b2",
+            s: "#362332",
+            t: "#e8c55d",
+            u: "#bd8787",
+            v: "#66c6a4",
+            w: "#21ec4b",
+            x: "#782364",
+            y: "#c3bf15",
+            z: "#3db35a"
         }));
         renderer.oritatami = config;
         return false;

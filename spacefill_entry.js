c6bb926f5f349e5a893fc50ab0e89f6dcc209d0a
Gyusun Yeom <omniavinco@gmail.com>
2016-11-24 19:04:41 +0000

    Static page generated

--- spacefill_entry.js
+++ spacefill_entry.js
@@ -61,32 +61,8 @@
 	$(document).ready(() => {
 	    let paperManager = new canvas_manager_1.CanvasManager("paper", 500, 500, 100);
 	    let renderer = new renderer_spacefill_1.SpaceFillRenderer(paperManager, new renderer_1.Theme({
-	        a: "#a69dd8",
-	        b: "#0c35b0",
-	        c: "#f82750",
-	        d: "#0ebd31",
-	        e: "#5fab4f",
-	        f: "#c538cf",
-	        g: "#014a59",
-	        h: "#e14af8",
-	        i: "#9fb730",
-	        j: "#4bec60",
-	        k: "#ef9345",
-	        l: "#d2ece0",
-	        m: "#9cda80",
-	        n: "#dbc07c",
-	        o: "#7328dd",
-	        p: "#1e9942",
-	        q: "#621b7b",
-	        r: "#c830b2",
-	        s: "#362332",
-	        t: "#e8c55d",
-	        u: "#bd8787",
-	        v: "#66c6a4",
-	        w: "#21ec4b",
-	        x: "#782364",
-	        y: "#c3bf15",
-	        z: "#3db35a"
+	        a: "#000",
+	        z: "#F00",
 	    }));
 	    renderer.drawGrid();
 	    renderer.createSpaceFillHTML();

73413dee5b625a2381904fdfceaa06187a94a4fd
Gyusun Yeom <omniavinco@gmail.com>
2016-11-24 19:48:09 +0000

    Static page generated

--- spacefill_entry.js
+++ spacefill_entry.js
@@ -17352,7 +17352,7 @@
 	                    this.absoluteDirections.push(i);
 	                    if (this.absoluteDirections.length > 1) {
 	                        const lastDirection = this.absoluteDirections[this.absoluteDirections.length - 2];
-	                        const relativeDirection = i - lastDirection + 3;
+	                        const relativeDirection = (i - lastDirection + 3) % 6;
 	                        this.relativeDirections.push(relativeDirection < 0 ? relativeDirection + 6 : relativeDirection);
 	                    }
 	                    else {

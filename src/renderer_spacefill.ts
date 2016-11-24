a3b5a4a958d400d8ba18f3dacc2d4b4e33950f64
Gyusun Yeom <omniavinco@gmail.com>
2016-11-25 01:02:56 +0900

    end button

--- src/renderer_spacefill.ts
+++ src/renderer_spacefill.ts
@@ -15,6 +15,8 @@ export class SpaceFillRenderer extends Renderer {
     _filler: Filler;
     _hover: RaphaelSet;
     input: SpaceFillInputManager;
+    _button: RaphaelSet;
+    _buttonGlow: RaphaelSet;
 
     constructor(canvas: CanvasManager, theme?: Theme) {
         super(canvas, theme);
@@ -23,6 +25,7 @@ export class SpaceFillRenderer extends Renderer {
             showInputDiv.html(this.input.sequenceToString());
         });
 
+        this.drawEndButton();
         this.drawGrid();
         this.input.initialClick();
     }
@@ -34,6 +37,26 @@ export class SpaceFillRenderer extends Renderer {
         if (config) {
             this._filler = config;
         }
+    }
+
+    drawEndButton() {
+        this._button = this.paper.set()
+        .push(this.paper.rect(2, 2, 100, 20, 1).toFront().attr({fill: "red", stroke: "red 1px", opacity: 0.5}))
+        .push(this.paper.text(52, 12, "INPUT END"))
+        .mouseover(() => {
+            this._buttonGlow = this._button.glow({
+                width: 5, fill: true, opacity: 0.7
+            });
+        }).mouseout(() => {
+            if (this._buttonGlow) {
+                this._buttonGlow.remove();
+            }
+        }).click(() => this.onInputEnded());
+    }
+
+    onInputEnded() {
+        // input end!
+        alert("End!");
     }
 
     drawMouseMove() {
